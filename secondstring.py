4f09a6de75b015c2d2025531fc42b72c9ab8b156
Donal Maher <dm0860777682@gmail.com>
2020-03-11 17:26:41 +0000

    The code is edited on the review comments.

--- secondstring.py
+++ secondstring.py
@@ -1,24 +1,19 @@
 #G00387859 DONAL MAHER
 """
+Version 2. Edited based on retorspective and review comments.
 # Write a program that takes askes a user
-# to input a string  and outputs every seconnd letter in reverse order. 
+# to input a string  and outputs every seconnd letter in reverse order.
+# References: 1. w3Schools.2020. How to reverse a string. 1st edition. O'Reiliy Media. Sebastopol, CA United States of America.
+              2.Jake VanderPlas.2016.A whirlwind Tour of Python  https://www.oreilly.com/programming/free/files/a-whirlwind-tour-of-python.pdf  
 """
 print("Loop over String program ")
 try:
-    #Ask user for input
-    UserSentence = input("Please enter a sentence: ")
-    # Get the lenght of the sentence
-    senlength = len(UserSentence)
-    #Reverse the users input string
-    RevUserSen = UserSentence[senlength::-1]
-    RevSentence = []
-    #Iterate over the user input string and print out every 2nd element
-    #### string[start:stop:step size]####
-    for x in RevUserSen[0:senlength:2]:
-        RevSentence.append(x)
-    #printing the list using * and sep operator
-    print(*RevSentence, sep = " ")
-  
+    def revString(userInput):
+        # return every second letter
+        return userInput[::-2]
+    userInput = input("Please enter a sentence:")
+    print(revString(userInput))
+
 except Exception:
     #This will catch the excecption. The program will not fall over if this user does not enter string values. 
     print("sorry, somthing went wrong!")
